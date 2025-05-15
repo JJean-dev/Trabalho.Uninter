01344e18b6df120bf5a1f7ecc3471f9111bbb415
Jean Henrique <jjean.henriquee@gmail.com>
2025-05-15 20:33:43 -0300

    mensagem

---
+++ script.js
@@ -0,0 +1,7 @@
+let mensagemBotao = document.querySelector('#mensagemBotao');
+let mensagem = document.querySelector('#mensagem');
+
+mensagemBotao.addEventListener('click', function() {
+    mensagem.textContent = "Acredite em você mesmo e nada será impossível. O sucesso começa quando você decide tentar!";
+});
+;
